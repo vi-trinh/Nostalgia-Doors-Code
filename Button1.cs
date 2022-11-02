3e9df0e7ec6c5d34c9f80cd11fc705522eda5cc3
vi-trinh <93283345+vi-trinh@users.noreply.github.com>
2022-11-02 22:35:02 +0000

    Add files via upload

---
+++ Button1.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button1 : MonoBehaviour { 
+
+    public void door1 ()
+    {
+        Debug.Log("ONEdoor");
+
+        SceneManager.LoadScene("Scene1", LoadSceneMode.Single);
+    }
+
+    public void door2()
+    {
+        Debug.Log("TWOdoor");
+
+        SceneManager.LoadScene("Scene2", LoadSceneMode.Single);
+    }
+	
+	    public void door3()
+    {
+        Debug.Log("TWOdoor");
+
+        SceneManager.LoadScene("Scene3", LoadSceneMode.Single);
+    }
+	
+	    public void door4()
+    {
+        Debug.Log("TWOdoor");
+
+        SceneManager.LoadScene("Scene4", LoadSceneMode.Single);
+    }
+	
+	    public void door5()
+    {
+        Debug.Log("TWOdoor");
+
+        SceneManager.LoadScene("Scene5", LoadSceneMode.Single);
+    }
+	
+	    public void door6()
+    {
+        Debug.Log("TWOdoor");
+
+        SceneManager.LoadScene("Scene6", LoadSceneMode.Single);
+    }
+	
+	    public void door7()
+    {
+        Debug.Log("TWOdoor");
+
+        SceneManager.LoadScene("Scene7", LoadSceneMode.Single);
+    }
+	
+	    public void door8()
+    {
+        Debug.Log("TWOdoor");
+
+        SceneManager.LoadScene("Scene8", LoadSceneMode.Single);
+    }
+	
+	    public void door9()
+    {
+        Debug.Log("TWOdoor");
+
+        SceneManager.LoadScene("Scene9", LoadSceneMode.Single);
+    }
+	
+	    public void door10()
+    {
+        Debug.Log("TWOdoor");
+
+        SceneManager.LoadScene("Scene10", LoadSceneMode.Single);
+    }
+	
+	    public void door11()
+    {
+        Debug.Log("TWOdoor");
+
+        SceneManager.LoadScene("Scene11", LoadSceneMode.Single);
+    }
+	
+	    public void door12()
+    {
+        Debug.Log("TWOdoor");
+
+        SceneManager.LoadScene("Scene12", LoadSceneMode.Single);
+    }
+	
+	    public void door13()
+    {
+        Debug.Log("TWOdoor");
+
+        SceneManager.LoadScene("Scene13", LoadSceneMode.Single);
+    }
+	
+	    public void doorEND()
+    {
+        Debug.Log("TWOdoor");
+
+        SceneManager.LoadScene("End", LoadSceneMode.Single);
+    }
+	
+	    public void doorTITLE()
+    {
+        Debug.Log("TWOdoor");
+
+        SceneManager.LoadScene("Title", LoadSceneMode.Single);
+    }
+	
+}
---
+++ PlayScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayScript : MonoBehaviour
+{
+	void Update()
+	{
+		if (Input.GetKey("escape"))
+		{
+			Debug.Log("QUIT");
+
+			Application.Quit();
+		}
+	}
+}
